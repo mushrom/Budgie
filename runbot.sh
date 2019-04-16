840133325594e1c06500538edde222dea9fff074
My Name <foo@lisp.us.to>
2019-04-16 18:45:40 -0500

    speleng

--- runbot.sh
+++ runbot.sh
@@ -4,6 +4,6 @@ USERNAME=Budgie
 APIKEY="$(cat apikey$MODE.txt)"
 
 gtp2ogs --username $USERNAME --apikey $APIKEY $MODE --boardsize 9 --debug \
-	--greeting "Hi, have fun! If you run into any problems, such as the bot being glitchy or timing out, please contact my admin." \
+	--greeting "Hi, have fun! If you run into any problems such as the bot being glitchy or timing out, please contact my admin." \
 	--farewell "Thanks for the game!" \
 	-- ./thing
