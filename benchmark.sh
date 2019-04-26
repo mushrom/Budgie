c7d76a9b4e79d00f5f5073fa12b934de0f4a3476
My Name <foo@lisp.us.to>
2019-04-26 17:07:26 -0400

    Adding simple benchmarking script

---
+++ benchmark.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+for playouts in 1000 2000 3000 4000 5000 7500 10000; do
+	echo; echo; echo "$playouts playouts:"
+	time echo -e "genmove b\nquit\n" | ./thing --playouts $playouts 2 $@ &> /dev/null
+done
