28a54d2c98abf7b51f99b6fc774d1860fabfcbbc
My Name <foo@lisp.us.to>
2021-12-18 08:18:48 -0500

    Checking if stone last placed results in an atari, try capturing it if so

--- src/bot/capture_weighted_playout.cpp
+++ src/bot/capture_weighted_playout.cpp
@@ -10,6 +10,26 @@ coordinate capture_weighted_playout::apply(board *state) {
 	point::color other = other_player(state->current_player);
 	std::list<group*>& ataris = state->group_liberties[other][1];
 
+	if (state->moves == 0) {
+		// nothing to do on the first move
+		return {0, 0};
+	}
+
+	// check if the group of the opponent's last move is in atari,
+	// do a coin flip to determine whether to capture it
+	group *g = *(state->groups + state->coord_to_index(state->last_move));
+	if (g && g->liberties.size() == 1 && (rand()&1) == 1) {
+		coordinate temp = *g->liberties.begin();
+
+		if (!state->is_valid_coordinate(temp)) {
+			printf("invalid liberty at (%d, %d)?\n", temp.first, temp.second);
+		}
+
+		if (state->is_valid_move(temp)) {
+			return temp;
+		}
+	}
+
 	// TODO: probability here should be configurable
 	if (rand() % 6 == 0 && !ataris.empty()) {
 		// pick a random group to capture
